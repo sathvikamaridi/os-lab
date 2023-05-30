e0ac7698a885b28c64e7ce85daba8be22e644aa0
sathvikamaridi <120960220+sathvikamaridi@users.noreply.github.com>
2023-05-30 10:07:39 +0530

    Add files via upload

---
+++ write a sentence to a file.py
@@ -0,0 +1,8 @@
+with open('file.txt', 'w') as f:
+	# Define the data to be written
+	data = ['This is the first line', 'This is the second line', 'This is the third line']
+	# Use a for loop to write each line of data to the file
+	for line in data:
+		f.write(line + '\n')
+		# Optionally, print the data as it is written to the file
+		print(line)
